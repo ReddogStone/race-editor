036fc733314bf7ddf0bebbbf6a086e4e20ef960a
Reddogstone <thereddog@gmx.net>
2016-02-13 20:08:21 +0100

    Implemented a proper store without working list implementation yet

--- utils/store.js
+++ utils/store.js
@@ -1,85 +1,66 @@
+'use strict';
+
 var nodes = [];
 var currentTransaction = null;
-var nextId = 0;
 
-function derive(parent, func) {
-	var cached;
-	var node = makeNode(function deriveGetter(hot, available) {
-		let parentValues = available.get(parent);
-		if (parentValues) {
-			let result = {
-				value: cached
-			};
-			if (hot.has(parent)) {
-				value = func.apply(null, parentValues);
-				cached = value;
-			}
-			return result;
+function makeNode(parents, updateFunc) {
+	let handlers = [];
+	let initial = update();
+
+	function update() {
+		let parentValues = parents.map(parent => parent._values);
+		if (parentValues.every(values => !!values)) {
+			let values = updateFunc(parentValues);
+		 	handlers.forEach(handler => handler.apply(null, values));
+		 	return values;
 		}
-	});
-}
-
-function merge(parents) {
-	if (!Array.isArray(parents)) {
-		parents = Array.prototype.slice.call(parents);
 	}
 
-	var node = makeNode(function mergeGetter(hot, available) {
-		let parentValues = parents.map(parent => available.get(parent));
-		let available = parentValues.every(values => !!values);
-		let hot = parents.some(parent => hot.has(parent));
+	let node = {
+		_values: initial,
+		_parents: parents,
 
-		if (available) {
-			if (hot) {
+		_update: function() {
+			node._values = update();
+		},
 
+		bind: function(handler) {
+			handlers.push(handler);
+			if (node._values) {
+				handler.apply(null, node._values);
 			}
+		},
+
+		derive: function(func) {
+			return makeDerive(node, func);
+		},
+		filter: function(func) {
+			return makeFilter(node, func);
+		}
+	};
+
+	nodes.push(node);
+
+	return node;
+}
+
+function makeDerive(parent, func) {
+	return makeNode([parent], function deriveUpdate() {
+		return [func.apply(null, parent._values)];
+	});
+}
+
+function makeFilter(parent, func) {
+	return makeNode([parent], function filterUpdate() {
+		if (func.apply(null, parent._values)) {
+			return parent._values;
 		}
 	});
 }
 
-function makeNode(getter) {
-	var handlers = [];
-
-	return {
-		private: { get: getter }
-		notify: function(values) {
-			handlers.forEach(function(handler) {
-				handler.apply(null, values);
-			});
-		},
-		interface: {
-			on: function(handler) {
-				handlers.push(handler);
-			},
-			derive: function(func) {
-				return derive(getter);
-			}
-		}
-	};
-}
-
-exports.state = function(initial) {
-	var value = initial;
-
-	var node = makeNode();
-
-	var result = Object.create(node.interface);
-	result.update = function(pattern) {
-		for (let key in pattern) {
-			value[key] = pattern[key];
-		}
-		currentTransaction(node.private, value);
-	};
-	result.set = function(newValue) {
-		value = newValue;
-		currentTransaction(node.private, value);
-	};
-	return result;
-};
-
-function propagate(valueMap) {
-	let hot = new Set(valueMap.keys());
-	let available = valueMap;
+function propagate(changedStates) {
+	let hot = changedStates;
+	let available = new Set(changedStates);
 	let unknown = nodes.filter(node => !hot.has(node));
 
 	let active = true;
@@ -88,12 +69,18 @@ function propagate(valueMap) {
 		for (let i = unknown.length - 1; i >= 0; i--) {
 			let node = unknown[i];
 
-			let result = node.get(hot, available);
-			if (result) {
-				available.set(node, result.values);
-				if (result.hot) {
+			let parents = node._parents;
+
+			let nodeAvailable = parents.every(parent => available.has(parent));
+			let nodeHot = parents.some(parent => hot.has(parent));
+
+			if (nodeAvailable) {
+				if (nodeHot) {
+					node._update(hot);
 					hot.add(node);
 				}
+				available.add(node);
+
 				unknown.splice(i, 1);
 				active = true;
 			}
@@ -101,17 +88,66 @@ function propagate(valueMap) {
 	}
 }
 
+exports.merge = function(parents) {
+	if (!Array.isArray(parents)) {
+		parents = Array.prototype.slice.call(arguments);
+	}
+
+	return makeNode(parents, function mergeUpdate() {
+		let parentValues = parents.map(parent => parent._values);
+		return parentValues.reduce((memo, next) => memo.concat(next));
+	});
+};
+
+exports.dependent = function(parents, func) {
+	return exports.merge(parents).derive(func);
+};
+
+exports.state = function(initial) {
+	let value = initial;
+
+	let result = makeNode([], function updateState() {
+		return [value];
+	});
+	result.set = function(newValue) {
+		value = newValue;
+		result._update();
+		currentTransaction(result);
+	};
+	result.update = function(pattern) {
+		for (let key in pattern) {
+			value[key] = pattern[key];
+		}
+		result._update();
+
+		currentTransaction(result);
+	};
+	return result;
+};
+
+exports.list = function(initial) {
+	let result = exports.state(initial);
+	result.updateElement = function(index, pattern) {
+		let element = result._values[0][index];
+		for (let key in pattern) {
+			element[key] = pattern[key];
+		}
+		result._update([]);
+	};
+	return result;
+};
+
 exports.transaction = function(scope) {
 	if (currentTransaction) {
 		return scope();
 	}
 
-	let valueMap = new Map();
-	currentTransaction = function(node, value) {
-		valueMap.set(node, value);
+	let changedStates = new Set();
+	currentTransaction = function(node) {
+		changedStates.add(node);
 	};
 	scope();
 	currentTransaction = null;
 
-	propagate(valueMap);
+	propagate(changedStates);
 };
