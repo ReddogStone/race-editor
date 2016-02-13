3b5360fa9c4e25927db7c36f91bd2fbcf8063f96
Reddogstone <thereddog@gmx.net>
2016-02-13 07:25:32 +0100

    Added basic menu

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const Menu = require('menu');
 
 var mainWindow = null;
 
@@ -12,10 +13,25 @@ app.on('window-all-closed', function() {
 	}
 });
 
+const menuTemplate = [
+	{
+		label: 'File',
+		submenu: [
+			{
+				label: 'Close',
+				accelerator: 'Ctrl+W',
+				role: 'close'
+			}
+		]
+	}
+];
+
 app.on('ready', function() {
 	mainWindow = new BrowserWindow({ width: 800, height: 600 });
 	mainWindow.loadURL('file://' + __dirname + '/windows/main/index.html');
 	mainWindow.webContents.openDevTools();
+	mainWindow.setMenu(Menu.buildFromTemplate(menuTemplate));
+	mainWindow.maximize();
 
 	mainWindow.on('closed', function() {
 		mainWindow = null;
