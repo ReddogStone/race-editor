901b522c18ca03c2b27548ab6af88fd07c0d3f4a
Reddogstone <thereddog@gmx.net>
2016-02-20 18:16:35 +0100

    Fixed a bug with lambdas in the argument parser

--- utils/arguments-parser.js
+++ utils/arguments-parser.js
@@ -1,10 +1,20 @@
 'use strict';
 
 module.exports = function(func) {
-	let params = func.toString().match(/\(([^\)]*)\)/)[1].trim();
-	if (!params) {
-		return [];
+	let string = func.toString();
+	let params = null;
+
+	let match = string.match(/^\s*(\w+)\s*=>/);
+	if (match) {
+		params = match[1].trim();
+	} else {
+		match = string.match(/\(([^\)]*)\)/);
+		if (match) {
+			params = match[1].trim();
+		}
 	}
+
+	if (!params) { return []; }
 
 	return params.split(/,\s*/);
 };
